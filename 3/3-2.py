cf379e3581a91b4cb08f227b718976993e37a518
hyunghhs48 <hyunghhs98@gmail.com>
2023-01-31 05:27:34 +0000

    3-2.py

--- 3/3-2.py
+++ 3/3-2.py
@@ -1,8 +1,8 @@
 # N, M, K를 공백으로 구분하여 입력받기
-n, m, k = map(int, input().split())
+n, m, k = map(int,input().spilt())
 
 #N개의 수를 공백으로 구분하여 입력받기
-data = list(map(int,input().split()))
+data = list(map(int,input().spilt()))
 
 data.sort() #입력받은 수 정렬
 first = data[n-1] #가장 큰 수
