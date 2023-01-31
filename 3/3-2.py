1f2c16866261ce4780d9b4878e7ee53f63213185
hyunghhs48 <43942654+hyunghhs48@users.noreply.github.com>
2023-01-31 05:30:59 +0000

    update

--- 3/3-2.py
+++ 3/3-2.py
@@ -1,16 +1,17 @@
 # N, M, K를 공백으로 구분하여 입력받기
-n, m, k = map(int,input().spilt())
+n, m, k = map(int,input().split())
 
 #N개의 수를 공백으로 구분하여 입력받기
-data = list(map(int,input().spilt()))
+data = list(map(int,input().split()))
 
 data.sort() #입력받은 수 정렬
 first = data[n-1] #가장 큰 수
 second = data[n-2] #두 번째로 큰 수
 
 #가장 큰 수가 더해지는 횟수 계산
-count = int(m/(k+1))*k
-count += m %(k+1)
+count = int(m/(k+1))*k 
+# second 하나 포함해서 반복되는 수열 길이 : k+1, m/(k+1) : 수열 등장 횟수, m/(k+1)*k : first 등장 횟수 
+count += m % (k+1) # m/(k+1)이 딱 나눠지지 않을 때, 나머지로 남는 횟수
 
 result = 0
 result += (count) * first #가장 큰 수 더하기
